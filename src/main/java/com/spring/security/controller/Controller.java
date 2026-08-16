587a85714773b4715f4308682ff3a982118f2b39
AKXY006 <ak144336@gmail.com>
2026-08-17 04:14:23 +0530

    java with html

--- src/main/java/com/spring/security/controller/Controller.java
+++ src/main/java/com/spring/security/controller/Controller.java
@@ -1,14 +1,22 @@
 package com.spring.security.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Controller {
 	
 	@GetMapping
 	public String greet() {
-		return "hii";
+
+		// java with html return 
+		return """
+				<h1>yes</h1>
+				<p>no</>
+				
+			  """;
 	}
 
 }
