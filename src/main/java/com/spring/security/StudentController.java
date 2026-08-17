0303b9ae12ec38d731bfb8ce6954b2fdf6c98d25
AKXY006 <ak144336@gmail.com>
2026-08-17 13:38:11 +0530

    save new student use csrf-token

--- src/main/java/com/spring/security/StudentController.java
+++ src/main/java/com/spring/security/StudentController.java
@@ -3,8 +3,13 @@ package com.spring.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class StudentController {
@@ -15,10 +20,25 @@ public class StudentController {
 			  new Student(2, "Raj", 90)
 	));
 	
-	
 	@GetMapping("/student")
 	public List<Student> getStudents(){
 		return students;
+	}
+	
+	//generate csrf token
+	@GetMapping ("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+	
+	
+	//to add studnet when the dependencues of spring security will b/w remove
+	//to save new student use csrf-token
+	@PostMapping
+	public Student addStudent(@RequestBody Student student) {
+		students.add(student);
+		return student; 
 	}
 	
 
