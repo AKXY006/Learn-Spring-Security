651cc13b17aae4c4a38580b1290f25598187c08e
AKXY006 <ak144336@gmail.com>
2026-08-17 23:12:04 +0530

    check authentication

--- src/main/java/com/spring/security/config/config.java
+++ src/main/java/com/spring/security/config/config.java
@@ -4,10 +4,20 @@ import java.net.Authenticator.RequestorType;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.websocket.Session;
 
 @Configuration
 @EnableWebSecurity
@@ -19,21 +29,55 @@ public class config {
 	
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 		
 		//due to customizer disable all request are denied
-		httpSecurity.csrf(Customizer -> Customizer.disable());
-		httpSecurity.authorizeHttpRequests(request -> request.anyRequest().fullyAuthenticated());
+//		httpSecurity.csrf(Customizer -> Customizer.disable());
+//		httpSecurity.authorizeHttpRequests(request -> request.anyRequest().fullyAuthenticated());
 		
 		//Now we will customised the login page to open
 		//but in postman show login form
-		httpSecurity.formLogin(Customizer.withDefaults());
+//		httpSecurity.formLogin(Customizer.withDefaults());
 		
 		//authenticates the user and directly returns the controller’s output.
-		httpSecurity.httpBasic(Customizer.withDefaults());
+//		httpSecurity.httpBasic(Customizer.withDefaults());
 		
+		//same 
+		return httpSecurity
+				.csrf(Customizer -> Customizer.disable())
+				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(session ->
+				          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
 		
-		return  httpSecurity.build();
+	}
+	 
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		//set multiple user name and password 
+	    UserDetails user1 = User
+	            .withDefaultPasswordEncoder()
+	            .username("krrish")
+	            .password("krrish")
+	            .roles("Admin")
+	            .build();
+	    
+	    UserDetails user2 = User
+	            .withDefaultPasswordEncoder()
+	            .username("Admin")
+	            .password("root")
+	            .build();
+
+	    return new InMemoryUserDetailsManager(user2,user1);
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(null);
+		return provider;
+		
 	}
 
 }
